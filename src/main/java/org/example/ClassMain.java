587765498bdc3b46edb6813b903865209e95b479
alumnodavidfei@gmail.com <hjCDRfdhu142>
2022-12-04 11:26:35 +0100

    2

--- src/main/java/org/example/ClassMain.java
+++ src/main/java/org/example/ClassMain.java
@@ -3,6 +3,7 @@ package org.example;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.SpringApplication;
 //user:ben password:benspassword
+////http://localhost:8080/swagger-ui.html
 @SpringBootApplication
 public class ClassMain {
     public static void main(String[] args) {
--- src/main/java/org/example/controller/DemoController.java
+++ src/main/java/org/example/controller/DemoController.java
@@ -1,11 +1,13 @@
 package org.example.controller;
 
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
 
 @RestController
 public class DemoController {
-    @GetMapping("/")
+    @RequestMapping(method = RequestMethod.GET, value = "/api/javainuse")
     public String index() {
     return "Holaaa!!";
     }
